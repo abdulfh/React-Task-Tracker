df922c8dcafeb41d9a1b6fe8b830b38d7cddda53
abdul.fatah <abdul.fatah@ottodigital.id>
2021-10-20 17:19:24 +0700

    implement data source

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header'
+import Tasks from './components/Tasks';
+import { useState } from "react"
+import AddTask from './components/AddTask';
 
-function App() {
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+
+  const [tasks, setTask] = useState([
+    {
+      id: 1,
+      text: 'Shopping',
+      day: 'Feb 10 2021',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Fishing',
+      day: 'Feb 11 2021',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Jogging',
+      day: 'Feb 12 2021',
+      reminder: false
+    },
+  ])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    const newTask = { id, ...task }
+    setTask([...tasks, newTask])
+  }
+  // Delete Task
+  const deleteTask = (id) => {
+    setTask(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTask(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Task'}
     </div>
   );
 }
